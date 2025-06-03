6e7bbe6b8dad6559423a75c7d4a0fe4cf0fbc2c0
manjunathreddy1 <manjunathreddy1@gmail.com>
2025-06-03 22:15:02 +0530

    Update IPermtestProps.ts

--- IPermtestProps.ts
+++ IPermtestProps.ts
@@ -1,1 +1,8 @@
-https://<your-site>.sharepoint.com/sites/<site-name>/_api/web/lists/getbytitle('YourList')/items(<item-id>)
+  const item = await sp.web.lists.getByTitle("Site Pages").items.getById(pageItemId).select("Title", "FileRef")();
+
+    const pageTitle = item.Title;
+    const serverRelativeUrl = item.FileRef;
+    const absoluteUrl = `${context.pageContext.site.absoluteUrl}${serverRelativeUrl.replace(context.pageContext.web.serverRelativeUrl, '')}`;
+
+    console.log("Page Title:", pageTitle);
+    console.log("Full URL:", absoluteUrl);
