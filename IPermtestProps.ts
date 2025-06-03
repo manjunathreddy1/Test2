1884edaf4d847dcfa20f3b66dcd1960615695d78
manjunathreddy1 <manjunathreddy1@gmail.com>
2025-06-04 00:10:18 +0530

    Update IPermtestProps.ts

--- IPermtestProps.ts
+++ IPermtestProps.ts
@@ -1,8 +1,3 @@
- const currentUser = await sp.web.currentUser();
-
-    // Update the item
-    await sp.web.lists.getByTitle(listTitle).items.getById(itemId).update({
-      "Approval Status": "Approved",
-      "Approved ById": currentUser.Id, // use internal name + 'Id' for people field
-      "Approved Date": new Date().toISOString(),
-    });
+onst queryParams = new URLSearchParams(window.location.search);
+const page = queryParams.get("page");
+console.log("Page param:", page);
