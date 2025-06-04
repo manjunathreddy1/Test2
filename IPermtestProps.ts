f9a6acd3049c197e4b1a8078abe9f2e65cf58535
manjunathreddy1 <manjunathreddy1@gmail.com>
2025-06-04 20:24:05 +0530

    Update IPermtestProps.ts

--- IPermtestProps.ts
+++ IPermtestProps.ts
@@ -1,35 +1,6 @@
-interface PageItem {
-  id: string;
-  title: string;
-  fileName: string;
-  created: string;
-  modified: string;
-  createdBy: string;
-  authorField: {
-    Id: number;
-    Title: string;
-    Email?: string;
-    [key: string]: any;
-  };
+{
+  "__metadata": { "type": "SP.Data.Site_x0020_Page_x0020_ApprovalListItem" },
+  "Title": "Sample Page Approval",
+  "ApprovalStatus": "Pending",
+  "PageUrl": "https://yourtenant.sharepoint.com/sites/yoursite/SitePages/sample.aspx"
 }
-
-
-
-
-const mapped: PageItem[] = result.map((item: any) => ({
-  id: item.Id.toString(),
-  title: item.Title,
-  fileName: item.FileLeafRef,
-  created: new Date(item.Created).toLocaleString(),
-  modified: new Date(item.Modified).toLocaleString(),
-  createdBy: item.Author?.Title || "Unknown",
-  authorField: {
-    Id: item.Author?.Id,
-    Title: item.Author?.Title,
-    Email: item.Author?.Email,
-  },
-}));
-
-
-.select("Id", "Title", "FileLeafRef", "Created", "Modified", "Author/Id", "Author/Title", "Author/Email")
-.expand("Author")
