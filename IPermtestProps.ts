497e1470bd259ab3bde2a1d702d57737272f2cb0
manjunathreddy1 <manjunathreddy1@gmail.com>
2025-06-04 20:59:06 +0530

    Update IPermtestProps.ts

--- IPermtestProps.ts
+++ IPermtestProps.ts
@@ -1,10 +1,7 @@
-{
-  "__metadata": { "type": "SP.Data.Site_x0020_Page_x0020_ApprovalListItem" },
-  "Title": "Approval Example",
-  "ApprovedBy": {
-    "__metadata": { "type": "SP.FieldUserValue" },
-    "LookupId": 0,
-    "Email": "user@domain.com",
-    "LoginName": "i:0#.f|membership|user@domain.com"
-  }
-}
+_api/web/siteusers/getbyemail('user@domain.com')
+Extract the ID:
+
+plaintext
+Copy
+Edit
+body('Get_User')?['d']?['Id']
