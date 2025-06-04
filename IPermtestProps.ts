83df3b364deadef1fbfbfee3e1cc6ab5fb826755
manjunathreddy1 <manjunathreddy1@gmail.com>
2025-06-05 03:59:25 +0530

    Update IPermtestProps.ts

--- IPermtestProps.ts
+++ IPermtestProps.ts
@@ -1,13 +1,20 @@
-const textareaRef = React.useRef<HTMLTextAreaElement>(null);
+import * as React from 'react';
+import { Textarea, Button } from '@fluentui/react-components';
 
-const handleClick = () => {
-  const value = textareaRef.current?.value;
-  console.log("Text area value:", value);
+const MyComponent = () => {
+  const [textValue, setTextValue] = React.useState("");
+
+  const handleClick = () => {
+    console.log("Text area value:", textValue);
+  };
+
+  return (
+    <>
+      <Textarea
+        value={textValue}
+        onChange={(ev, data) => setTextValue(data.value)}
+      />
+      <Button onClick={handleClick}>Log Value</Button>
+    </>
+  );
 };
-
-return (
-  <>
-    <Textarea ref={textareaRef} />
-    <Button onClick={handleClick}>Log Value</Button>
-  </>
-);
