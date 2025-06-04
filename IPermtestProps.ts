94c8ec00016d27554e8e4a3ba3c4bf3696cad969
manjunathreddy1 <manjunathreddy1@gmail.com>
2025-06-05 03:41:21 +0530

    Update IPermtestProps.ts

--- IPermtestProps.ts
+++ IPermtestProps.ts
@@ -1,7 +1,81 @@
-_api/web/siteusers/getbyemail('user@domain.com')
-Extract the ID:
+import * as React from "react";
+import { Textarea } from "@fluentui/react-components";
 
-plaintext
-Copy
-Edit
-body('Get_User')?['d']?['Id']
+export const MyComponent = () => {
+  const [textValue, setTextValue] = React.useState("");
+
+  const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTextValue(event.target.value);
+  };
+
+  const getTextValue = () => {
+    alert("Textarea value: " + textValue);
+  };
+
+  return (
+    <>
+      <Textarea
+        value={textValue}
+        onChange={handleTextChange}
+        placeholder="Enter some text"
+      />
+      <button onClick={getTextValue}>Get Text</button>
+    </>
+  );
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+import * as React from 'react';
+import { Button } from '@fluentui/react-components';
+import { ArrowLeft16Filled, ArrowRight16Filled } from '@fluentui/react-icons';
+
+const PaginationButtons = () => {
+  const [currentPage, setCurrentPage] = React.useState(1);
+
+  const handlePrevious = () => {
+    if (currentPage > 1) {
+      setCurrentPage(prev => prev - 1);
+    }
+  };
+
+  const handleNext = () => {
+    setCurrentPage(prev => prev + 1);
+  };
+
+  return (
+    <div className="flex gap-4 items-center">
+      <Button
+        appearance="secondary"
+        icon={<ArrowLeft16Filled />}
+        onClick={handlePrevious}
+        disabled={currentPage === 1}
+      >
+        Previous
+      </Button>
+
+      <span>Page {currentPage}</span>
+
+      <Button
+        appearance="primary"
+        icon={<ArrowRight16Filled />}
+        iconPosition="after"
+        onClick={handleNext}
+      >
+        Next
+      </Button>
+    </div>
+  );
+};
+
+export default PaginationButtons;
